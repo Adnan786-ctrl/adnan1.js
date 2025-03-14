a522a836c2177f1d2399085725323e23be0f419f
Adnan786-ctrl <adnanqureshi0489@gmail.com>
2025-03-14 10:22:03 +0000

    conversions of datatypes

---
+++ .devcontainer/01 basics/conversionsoperations.js
@@ -0,0 +1,23 @@
+
+let score = "35adc"
+let word = null
+
+let valueInNumber = Number(null)
+
+console.log(typeof word)
+
+console.log(typeof score)
+console.log(typeof (score))
+
+//let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+let IsLoggedIn = "Adnan"
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(IsLoggedIn);
+
+let somenumber = 35
+let stringNumber = String(somenumber)
+console.log(typeof stringNumber);
---
+++ .devcontainer/01 basics/txts.js
@@ -0,0 +1,12 @@
+let name = "Adnan"
+/* the main datatypes in javascript is let ,const and var
+but the changes in var & let is thst if the change is occured in var in the file
+code  the whole var will be changes so that the problem is resolevd by the let datatypes  
+
+
+*/
+
+console.log(typeof "Adnan")
+
+console.log(typeof null)
+console.log(typeof undefined)
